58e3c0eb2e17be1ce767d4590eb4506d45e598c6
Michał Worwąg <michalwo@gmail.com>
2019-01-19 18:06:20 +0100

    App finished

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React, { Component } from 'react';
-import './App.scss';
+import React, { Component } from "react";
+import Navbar from "./components/Navbar/Navbar";
+import TourList from "./components/TourList/TourList";
+
+import "./App.scss";
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello</h1>
+      <div>
+        <Navbar />
+        <TourList />
+      </div>
     );
   }
 }
