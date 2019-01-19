29e34ed9fee4c1298073cbb5825611c9d20219fb
Michał Worwąg <michalwo@gmail.com>
2019-01-19 18:14:07 +0100

    Updates

---
+++ precache-manifest.3e0ba61201afab62841f01ca03d40c9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "24e781562a3a3a9ed91f",
+    "url": "/city-tours/static/css/main.f137fa40.chunk.css"
+  },
+  {
+    "revision": "24e781562a3a3a9ed91f",
+    "url": "/city-tours/static/js/main.24e78156.chunk.js"
+  },
+  {
+    "revision": "f03e8ae863144a61c620",
+    "url": "/city-tours/static/js/1.f03e8ae8.chunk.js"
+  },
+  {
+    "revision": "1c1a25b7e40769b5ce82",
+    "url": "/city-tours/static/js/runtime~main.1c1a25b7.js"
+  },
+  {
+    "revision": "2fa820ba2de1e0d179b5ba1befefa290",
+    "url": "/city-tours/index.html"
+  }
+];
